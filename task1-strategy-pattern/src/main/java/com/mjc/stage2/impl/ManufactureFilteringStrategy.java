32145b1da644559fe50004c8bfc2c0eddeae8ba5
ShulpinP <96373490+ShulpinP@users.noreply.github.com>
2022-12-14 11:44:02 +0300

    easy

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/ManufactureFilteringStrategy.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/ManufactureFilteringStrategy.java
@@ -1,11 +1,17 @@
 package com.mjc.stage2.impl;
 
 
+import com.mjc.stage2.FilteringStrategy;
+import com.mjc.stage2.Product;
+import com.mjc.stage2.ShopStock;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class ManufactureFilteringStrategy {
+public class ManufactureFilteringStrategy implements FilteringStrategy {
     private String manufacture;
 
-    // Write your code here!
+    @Override
+    public boolean filter(Product product) {
+        return product.getManufacture().equalsIgnoreCase(manufacture);
+    }
 }
